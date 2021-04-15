76791c23a5f2243b6a41c8fc14615305e80f65a0
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-04-15 11:10:13 +0000

    media/rc: Send sync space information on the lirc device

--- docs/conf.py
+++ docs/conf.py
@@ -134,3 +134,155 @@ html_theme_path = ['_themes']
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = '_static/konlpy.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#html_favicon = None
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_style = 'konlpy.css'
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+#html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+#html_use_smartypants = True
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    'index': ['github-banner.html', 'sidebar-header.html', 'localtoc.html', 'translations.html', 'sidebar-footer.html', 'searchbox.html'],
+    '**'   : ['github-banner.html', 'localtoc.html', 'translations.html', 'relations.html']
+}
+# Additional templates that should be rendered to pages, maps page names to
+# template names.
+#html_additional_pages = {}
+
+# If false, no module index is generated.
+#html_domain_indices = True
+
+# If false, no index is generated.
+#html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+#html_split_index = False
+
+# If true, links to the reST sources are added to the pages.
+#html_show_sourcelink = True
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+#html_show_sphinx = True
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+#html_show_copyright = True
+
+# If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#html_use_opensearch = ''
+
+# This is the file name suffix for HTML files (e.g. ".xhtml").
+#html_file_suffix = None
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'KoNLPydoc'
+
+rst_prolog = f"""
+.. |release| replace:: v{release}
+"""
+
+# -- Options for LaTeX output --------------------------------------------------
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+'papersize': 'a4paper',
+'classoptions': ',openany,oneside',
+'babel': '\\usepackage[english]{babel}',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+'preamble': '\\usepackage{kotex}',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+  ('index', 'KoNLPy.tex', u'KoNLPy Documentation', u'Lucy Park', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+latex_logo = '_static/konlpy.png'
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+latex_use_parts = False
+
+# If true, show page references after internal links.
+latex_show_pagerefs = True
+
+# If true, show URL addresses after external links.
+latex_show_urls = True
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+
+# -- Options for manual page output --------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'konlpy', u'KoNLPy Documentation', [u'Lucy Park'], 1)
+]
+
+# If true, show URL addresses after external links.
+#man_show_urls = False
+
+
+# -- Options for Texinfo output ------------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  ('index', 'KoNLPy', u'KoNLPy Documentation',
+   u'Lucy Park', 'KoNLPy', 'One line description of project.',
+   'Miscellaneous'),
+]
+
+# Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+
+# If false, no module index is generated.
+#texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+#texinfo_show_urls = 'footnote'
+
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'nltk': ('http://nltk.org', None),
+    'python': ('http://docs.python.org/', None),
+}
+
+extlinks = {
+    'commit': ('https://github.com/konlpy/konlpy/commit/%s', ''),
+    'committer': ('https://github.com/%s', ''),
+    'issue': ('https://github.com/konlpy/konlpy/issues/%s', '#'),
+}
+
+# i18n
+locale_dirs = ['locale']
+gettext_compact = True
---
+++ docs/examples/chunking.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import konlpy
+import nltk
+
+# POS tag a sentence
+sentence = u'만 6세 이하의 초등학교 취학 전 자녀를 양육하기 
