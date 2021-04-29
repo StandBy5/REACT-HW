9382f506bd36f57aa4234df0927f215f8dd9dcb5
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-04-29 09:48:47 +0000

    add design

--- docs/examples/collocations.py
+++ docs/examples/collocations.py
@@ -3,4 +3,17 @@
 
 from konlpy.tag import Kkma
 from konlpy.corpus import kolaw
-from k
+from konlpy.utils import pprint
+from nltk import collocations
+
+
+measures = collocations.BigramAssocMeasures()
+doc = kolaw.open('constitution.txt').read()
+
+print('\nCollocations among tagged words:')
+tagged_words = Kkma().pos(doc)
+finder = collocations.BigramCollocationFinder.from_words(tagged_words)
+pprint(finder.nbest(measures.pmi, 10)) # top 5 n-grams with highest PMI
+
+print('\nCollocations among words:')
+words
