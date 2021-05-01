d9a3e22b4cabc7c29355673d15c877bb592ae494
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-05-01 13:10:23 +0000

    Refactor comments

--- docs/examples/collocations.py
+++ docs/examples/collocations.py
@@ -16,4 +16,13 @@ finder = collocations.BigramCollocationFinder.from_words(tagged_words)
 pprint(finder.nbest(measures.pmi, 10)) # top 5 n-grams with highest PMI
 
 print('\nCollocations among words:')
-words
+words = [w for w, t in tagged_words]
+ignored_words = [u'안녕']
+finder = collocations.BigramCollocationFinder.from_words(words)
+finder.apply_word_filter(lambda w: len(w) < 2 or w in ignored_words)
+finder.apply_freq_filter(3) # only bigrams that appear 3+ times
+pprint(finder.nbest(measures.pmi, 10))
+
+print('\nCollocations among tags:')
+tags = [t for w, t in tagged_words]
+finder = collocations.BigramCollocationFinder.from_words(tags
