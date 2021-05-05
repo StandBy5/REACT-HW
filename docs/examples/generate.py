153d9052ddc1803f36bdac06b45019208a49e276
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-05-05 11:28:25 +0000

    Add notebook

--- docs/examples/generate.py
+++ docs/examples/generate.py
@@ -2,3 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import bisect
+import itertools
+import random
+
+import nltk
+from konlpy.corpus import kolaw
+from konlpy.tag import Mecab # MeCab tends to reserve the original form of morphemes
+
+
+def generate_sentence(cfdist, word, num=15):
+    sentence = []
+
+    # Generate words until we meet a period
+    while word!='.':
+        sentence.append(word)
+
+        # Generate the next word based on probability
+        choices, weights = zip(*cfdist[word].items()
