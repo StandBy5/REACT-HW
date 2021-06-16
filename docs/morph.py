e93879d191b5a47ee8e88d0e63a5e27b801bacdf
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-06-16 08:27:56 +0000

    clean up code a bit

--- docs/morph.py
+++ docs/morph.py
@@ -1,1 +1,22 @@
-#! /us
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+from time import time
+
+from konlpy import tag
+from konlpy.corpus import kolaw
+from konlpy.utils import csvwrite, pprint
+
+
+def tagging(tagger, text):
+    r = []
+    try:
+        r = getattr(tag, tagger)().pos(text)
+    except Exception as e:
+        print "Uhoh,", e
+    return r
+
+
+def measure_time(taggers, mult=6):
+    doc = kolaw.open('constitution.txt').read()*6
+ 
