c3276a430451a7d245eb2fc5675f467adfcd2d1a
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-06-24 12:22:09 +0000

    Updating travis

--- konlpy/about.py
+++ konlpy/about.py
@@ -1,4 +1,24 @@
 # Inspired from:
 # https://packaging.python.org/guides/single-sourcing-package-version/
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
-# https://github.com/e
+# https://github.com/explosion/spaCy/blob/master/spacy/about.py
+
+__all__ = [
+    '__author__',
+    '__copyright__',
+    '__email__',
+    '__license__',
+    '__summary__',
+    '__title__',
+    '__url__',
+    '__version__',
+]
+
+__title__ = 'KoNLPy'
+__version__ = '0.6.0'
+
+__author__ = 'Team KoNLPy'
+__email__ = 'konlpy@googlegroups.com'
+__url__ = 'http://konlpy.org'
+
+__summary__ = 'Python package for Korean natural language processi
