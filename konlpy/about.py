30f0cf4cd1c6b9fa3457d06b3d2b3d0a407b281a
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-06-24 08:58:42 +0000

    v3.0

---
+++ konlpy/about.py
@@ -0,0 +1,4 @@
+# Inspired from:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+# https://github.com/e
