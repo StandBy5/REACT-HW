1f5fbb954dc1a3ff39f824523c53b78c786cd0a1
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-07-08 21:59:45 +0000

    Convert to Swift 3

--- konlpy/downloader.py
+++ konlpy/downloader.py
@@ -19,4 +19,12 @@ from konlpy import internals
 
 def default_download_dir():
     """
-    Returns the directory t
+    Returns the directory to which packages will be downloaded by default.
+    This value can be overriden using the constructor,
+    or on a case-by-case basis using the ``download_dir`` argument
+    when calling ``download()``.
+
+    On Windows, the default download directory is ``PYTHONHOME/lib/konlpy``,
+    where *PYTHONHOME* is the directory containing Python e.g., ``C:\\Python27``.
+
+    On all other platf
