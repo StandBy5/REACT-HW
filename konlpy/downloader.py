915d264be4fc9b700b94f5a57f369930b6fe85c5
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2021-07-13 21:32:04 +0000

    swap: fix shmem swapping when more than 8 areas

--- konlpy/downloader.py
+++ konlpy/downloader.py
@@ -158,4 +158,14 @@ class Downloader(object):
             yield "[konlpy_data] '%s' is already installed" % info['id']
             return
 
-        # Check for (and remove) any old/st
+        # Check for (and remove) any old/stale version
+        filepath = os.path.join(download_dir, '%s.%s' % (info['filepath'], info['ext']))
+        if os.path.exists(filepath):
+            if status == self.STALE:
+                yield "[konlpy_data] This file is stale"
+            os.remove(filepath)
+
+        # Ensure the download_dir exists
+        if not os.path.exists(download_dir):
+            os.mkdir(download_dir)
+        subdir = o
