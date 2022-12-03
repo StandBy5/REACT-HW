d8a5b5e1d1d4ee93f4d89044dd81e270fca93333
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2022-12-03 12:27:43 +0000

    some more tests

---
+++ konlpy/java/src/kr/lucypark/jhannanum/comm/HannanumInterface.java
@@ -0,0 +1,13 @@
+package kr.lucypark.jhannanum.comm;
+
+/* Copyright 2014 Lucy Park <me@lucypark.kr> */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import kr.ac.kaist.swrc.jhannanum.comm.Eojeol;
+import kr.ac.kaist.swrc.jhannanum.comm.Sentence;
+import kr.ac.kaist
