0fad30ef47c1495f719962842f55d47a81a9d6a6
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2022-12-13 19:28:17 +0000

    MAGETWO-31654: Magento Ui module code base clean up

--- konlpy/tag/_mecab.py
+++ konlpy/tag/_mecab.py
@@ -12,4 +12,19 @@ from konlpy import utils
 from konlpy.tag._common import validate_phrase_inputs
 
 
-__all__ = ['Mec
+__all__ = ['Mecab']
+
+
+attrs = ['tags',        # 품사 태그
+         'semantic',    # 의미 부류
+         'has_jongsung',  # 종성 유무
+         'read',        # 읽기
+         'type',        # 타입
+         'first_pos',   # 첫번째 품사
+         'last_pos',    # 마지막 품사
+         'original',    # 원형
+         'indexed']     # 인덱스 표현
+
+
+def parse(result, allattrs=False, join=False, split_inflect=False):
+    def split(elem, join=False, split_inf
