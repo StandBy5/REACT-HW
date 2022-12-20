3b009fe5c4e58ecd4fc49b80e3f7863876610837
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2022-12-20 13:01:28 +0000

    Improved icons

--- konlpy/tag/_mecab.py
+++ konlpy/tag/_mecab.py
@@ -79,4 +79,10 @@ class Mecab():
         >>> from konlpy.tag import Mecab
         >>> mecab = Mecab()
         >>> print(mecab.morphs(u'영등포구청역에 있는 맛집 좀 알려주세요.'))
-        ['영등포구', '청역', '에', '
+        ['영등포구', '청역', '에', '있', '는', '맛집', '좀', '알려', '주', '세요', '.']
+        >>> print(mecab.nouns(u'우리나라에는 무릎 치료를 잘하는 정형외과가 없는가!'))
+        ['우리', '나라', '무릎', '치료', '정형외과']
+        >>> print(mecab.pos(u'자연주의 쇼핑몰은 어떤 곳인가?'))
+        [('자연', 'NNG'), ('주', 'NNG'), ('의', 'JKG'), ('쇼핑몰', 'NNG'), ('은', 'JX'), ('어떤', 'MM'), ('곳', 'NNG'), ('인가', 'VCP+EF'), ('?', 'SF')]
+
+    :param dicpath: The path of the MeCab-ko d
