aac12fe966077949851928318813c8d8b8b1ea1e
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2022-12-20 15:37:30 +0000

    Update render.js

--- konlpy/tag/_mecab.py
+++ konlpy/tag/_mecab.py
@@ -97,4 +97,9 @@ class Mecab():
             self.tagger = Tagger('-d %s' % dicpath)
             self.tagset = utils.read_json('%s/data/tagset/mecab.json' % utils.installpath)
         except RuntimeError:
-            raise Ex
+            raise Exception('The MeCab dictionary does not exist at "%s". Is the dictionary correctly installed?\nYou can also try entering the dictionary path when initializing the Mecab class: "Mecab(\'/some/dic/path\')"' % dicpath)
+        except NameError:
+            raise Exception('Install MeCab in order to use it: http://konlpy.org/en/latest/install/')
+
+    def __setstate__(self, state):
+        """just reiniti
