4fc5ebe8f9c3e2d0c81aadfbfae9672d39b813a4
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2022-12-25 13:53:34 +0000

    Fix version check

--- konlpy/tag/_okt.py
+++ konlpy/tag/_okt.py
@@ -3,4 +3,21 @@ from __future__ import absolute_import
 
 import jpype
 
-from konlpy import jvm
+from konlpy import jvm, utils
+from konlpy.tag._common import validate_phrase_inputs
+
+
+def Twitter(jvmpath=None):
+    """
+    The ``Twitter()`` backend has changed to ``Okt()`` since KoNLPy v0.5.0.
+    See :issue:`141` for details.
+    """
+
+    from warnings import warn
+    warn('"Twitter" has changed to "Okt" since KoNLPy v0.4.5.')
+    return Okt(jvmpath)
+
+
+class Okt():
+    """
+    Wrapper for `Open Korean Text <https://github.com/o
