e26e94d96eccbf604cef304a3377c6722edc0e73
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2022-12-26 17:03:01 +0000

    Update main.rb

--- konlpy/tag/_okt.py
+++ konlpy/tag/_okt.py
@@ -20,4 +20,15 @@ def Twitter(jvmpath=None):
 
 class Okt():
     """
-    Wrapper for `Open Korean Text <https://github.com/o
+    Wrapper for `Open Korean Text <https://github.com/open-korean-text/open-korean-text>`_.
+
+    Open Korean Text is an open source Korean tokenizer written in Scala,
+    developed by Will Hohyon Ryu.
+
+    .. code-block:: python
+
+        >>> from konlpy.tag import Okt
+        >>> okt = Okt()
+        >>> print(okt.morphs(u'단독입찰보다 복수입찰의 경우'))
+        ['단독', '입찰', '보다', '복수', '입찰', '의', '경우']
+        >>> print(okt.nouns(u'유일하게 항공기 체계 종합개발 경험을 갖고 있는 KAI는')
