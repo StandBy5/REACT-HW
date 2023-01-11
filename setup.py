ba37494aae5464f138dfad47f85219c26fcb5215
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2023-01-11 14:15:45 +0000

    USB: serial: cp210x: add Silicon Labs 358x VID and PID

--- setup.py
+++ setup.py
@@ -11,4 +11,18 @@ def get_about():
     about = {}
     basedir = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(basedir, 'konlpy', 'about.py')) as f:
-        exec(f.read
+        exec(f.read(), about)
+    return about
+
+
+def requirements():
+    # both JPype1 and JPype1-py3 don't support Windows. see http://konlpy.org/en/v0.4.4/install/.
+    if platform.system() == 'Windows':
+        return []
+
+    def _openreq(reqfile):
+        with open(os.path.join(os.path.dirname(__file__), reqfile)) as f:
+            return f.read().splitlines()
+
+    if sys.version_info[0] < 3:
+        return _openreq('requirements2.
