ce92fd777718d8906952c818889a03dc54525974
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2023-01-15 18:06:00 +0000

    Fix test warnings

--- test/test_hannanum.py
+++ test/test_hannanum.py
@@ -9,4 +9,17 @@ def hannanum_instance():
     from konlpy import init_jvm
     from konlpy.tag import Hannanum
     init_jvm()
-    h = Hannanum
+    h = Hannanum()
+    return h
+
+@pytest.fixture
+def string():
+    return u"꽃가마 타고 강남 가자!"
+
+
+def test_hannanum_analyze(hannanum_instance, string):
+    assert hannanum_instance.analyze(string) ==\
+        [[[(u'\uaf43\uac00', u'ncn'), (u'\uc774', u'jp'), (u'\ub9c8', u'ef')],
+          [(u'\uaf43\uac00\ub9c8', u'ncn')],
+          [(u'\uaf43\uac00', u'nqq'), (u'\uc774', u'jp'), (u'\ub9c8', u'ef')],
+         
