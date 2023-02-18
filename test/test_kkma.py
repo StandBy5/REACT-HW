077b3c7d9db4370c90fa43df3e53d45cab14a8bd
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2023-02-18 07:19:46 +0000

    Mods

--- test/test_kkma.py
+++ test/test_kkma.py
@@ -31,4 +31,18 @@ def test_kkma_pos(kkma_instance, string):
     assert kkma_instance.pos(string) ==\
         [(u'\uaf43\uac00\ub9c8', u'NNG'),
          (u'\ud0c0\uace0', u'NNG'),
-         (u'\uac15\ub0a8', u'NN
+         (u'\uac15\ub0a8', u'NNG'),
+         (u'\uac00\uc790', u'NNG'),
+         (u'!', u'SF')]
+
+def test_kkma_pos_join(kkma_instance, string):
+    assert kkma_instance.pos(string, join=True) ==\
+        [u'\uaf43\uac00\ub9c8/NNG',
+         u'\ud0c0\uace0/NNG',
+         u'\uac15\ub0a8/NNG',
+         u'\uac00\uc790/NNG',
+         u'!/SF']
+
+def test_kkma_sentences(kkma_instance, string):
+    assert kkma_instance.sentences(string) ==\
+        [u'\uaf43\ua
