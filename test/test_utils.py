d09a3e38f4ccb57cd4d454bbe9fbb7d2b968d8a0
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2023-02-19 11:43:14 +0000

    Change errs() to dbgs().

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,4 +2,15 @@
 # -*- coding: utf-8 -*-
 import sys
 
-d
+def test_utils_pprint(capsys): # Fixture `capsys` allows stdout/stderr captures
+    from konlpy.utils import pprint
+    pprint([u"저는 소프트웨어 관련학과 입니다."])
+    out, err = capsys.readouterr()
+    if sys.version_info[0] < 3:
+        assert out == u"[저는 소프트웨어 관련학과 입니다.]\n"
+    else:
+        assert out == u"['저는 소프트웨어 관련학과 입니다.']\n"
+
+def test_utils_concordance():
+    from konlpy.corpus import kolaw
+    from konlpy.utils  import c
