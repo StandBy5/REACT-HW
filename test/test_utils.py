404a2055e196680d42110b40aa145bead95381a8
StandBy5 <98222061+StandBy5@users.noreply.github.com>
2023-02-19 15:48:43 +0000

    Update Reference.md

--- test/test_utils.py
+++ test/test_utils.py
@@ -23,4 +23,5 @@ def test_utils_concordance_show(capsys):
     from konlpy.utils  import concordance
     doc = kolaw.open('constitution.txt').read()
     ccd = concordance(u'대한민국', doc, show=True)
-    out, err =
+    out, err = capsys.readouterr()
+    assert out == u"0\t대한민국헌법 유구한 역사와\n9\t대한국민은 3·1운동으로 건립된 대한민국임시정부의 법통과 불의에\n98\t총강 제1조 ① 대한민국은 민주공화국이다. ②대한민국의\n100\t① 대한민국은 민주공화국이다. ②대한민국의 주권은 국민에게\n110\t나온다. 제2조 ① 대한민국의 국민이 되는\n126\t의무를 진다. 제3조 대한민국의 영토는 한반도와\n133\t부속도서로 한다. 제4조 대한민국은 통일을 지향하며,\n147\t추진한다. 제5조 ① 대한민국은 국제평화의 유지에\n787\t군무원이 아닌 국민은 대한민국의 영역안에서는 중대한\n1836\t파견 또는 외국군대의 대한민국 영역안에서의 주류에\n3620\t경제 제119조 ① 대한민국의 경제질서는 개인과\n"
